dab712e3d94adcff3c7ca937490449baa570fa26
Nitish Kumar <vickyyadav5383@gmail.com>
2025-09-06 11:36:22 +0530

    basic setup done, all the basic file is added , and basic file structure is ready

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import {React,useEffect,useState} from 'react'
+import {link} from 'react-router-dom';
+
+import {styles} from '../styles';
+import {navlnks} from '../constants';
+import {logo, menu, close} from '../assets';
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
